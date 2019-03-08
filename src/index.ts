3243aed3b1f08192aa4b515d1045c8435cc2f12b
Andrzej Tracz <andrzej.tracz7@gmail.com>
2019-03-08 22:29:18 +0100

    tweak ts config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function hello() {
+    await delay(200);
     return "Hello World!";
 }
 
 export {
+    delay,
     hello
 }
